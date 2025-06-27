3e2f4916b1c845bbbcce460b5fd4f91dd95065e0
Karmpatel18 <karmpatel1203@gmail.com>
2025-06-27 22:50:19 +0530

    sm change

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,39 +1,39 @@
 import './App.css'
-import { useRef, useState } from 'react'
+import { useRef, useState, useEffect } from 'react'
 function App() {
-  const [ input , setInput ] = useState("");
+  const [input, setInput] = useState("");
+  const [socket, setSocket] = useState();
   const inputRef = useRef<HTMLInputElement>(null)
   function handleSummit() {
-    const i  = inputRef.current?.value
-    if(i){
+    const i = inputRef.current?.value
+    if (i) {
       setInput(i);
-    }else{
+      socket.send(i)
+    } else {
       return
     }
-    
+
   }
+  useEffect(() => {
+    const ws = new WebSocket('ws://localhost:8080');
+    //@ts-expect-error sdsfs
+    setSocket(ws)
+    
+    ws.onmessage = function (event) {
+      console.log('Message from server:', event.data);
+    };
 
-  const socket = new WebSocket('ws://localhost:8080');
-  socket.onopen = function (event) {
-    console.log('WebSocket connection opened:', event);
-    // You can now send messages
-    socket.send(input);
-  };
+    
+    ws.onclose = function (event) {
+      console.log('WebSocket connection closed:', event);
+    };
 
-  // Event listener for incoming messages from the server
-  socket.onmessage = function (event) {
-    console.log('Message from server:', event.data);
-  };
+    
+    ws.onerror = function (error) {
+      console.error('WebSocket error:', error);
+    };
+  }, [input])
 
-  // Event listener for when the connection is closed
-  socket.onclose = function (event) {
-    console.log('WebSocket connection closed:', event);
-  };
-
-  // Event listener for errors
-  socket.onerror = function (error) {
-    console.error('WebSocket error:', error);
-  };
   return (
     <>
       hi there
